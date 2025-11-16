b60242dd8035c9c3f1dda2462e7e1b0fe2d4a717
Choudhary <alka.choudhary.ext@siemens.com>
2025-11-17 00:05:14 +0530

    Use VITE_API_URL instead of localhost

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/tasks`;
 
 export const getTasks = async () => {
   const res = await fetch(API_URL);
